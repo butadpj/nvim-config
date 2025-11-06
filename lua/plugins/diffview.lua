f907c04851cb085b36daedbbeedb031e3b3968e5
butadpj <butadpj@gmail.com>
2025-11-07 02:31:49 +0800

    feat: add ability to open the file (under the cursor) in Diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -7,5 +7,49 @@ return {
 		{ "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
 		{ "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "(Current file) Diffview File History" },
 	},
-	config = true, -- Uses default settings
+	-- Open file under cursor and closes the DiffView
+	config = function()
+		local diffview = require("diffview")
+		local actions = require("diffview.actions")
+		local lib = require("diffview.lib")
+		local diffview_config = require("diffview.config")
+
+		local keymaps = vim.deepcopy(diffview_config.defaults.keymaps)
+
+		local function open_file_and_close_view()
+			local view = lib.get_current_view()
+			local diffview_tab = view and view.tabpage
+
+			actions.goto_file_edit()
+
+			if view and diffview_tab and vim.api.nvim_get_current_tabpage() ~= diffview_tab then
+				vim.schedule(function()
+					if view.tabpage and vim.api.nvim_tabpage_is_valid(view.tabpage) then
+						view:close()
+					end
+				end)
+			end
+		end
+
+		table.insert(keymaps.view, {
+			"n",
+			"<leader>do",
+			open_file_and_close_view,
+			{ desc = "Open file in current tab and close Diffview" },
+		})
+		table.insert(keymaps.file_panel, {
+			"n",
+			"<leader>do",
+			open_file_and_close_view,
+			{ desc = "Open file in current tab and close Diffview" },
+		})
+
+		diffview.setup({
+			keymaps = keymaps,
+		})
+
+		vim.api.nvim_create_user_command("DiffviewOpenFile", open_file_and_close_view, {
+			desc = "Open the Diffview file under the cursor in the editing tab",
+		})
+	end,
 }
