ac0cb2f29dba1d3a76c0fb188f45d90fb3cf6c59
butadpj <butadpj@gmail.com>
2026-02-25 13:01:48 +0800

    feat: add keybinding to copy file's path relative to project root

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,13 +13,29 @@ return {
 	keys = {
 		{ "<C-b>", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
 	},
-	opts = {
-		filesystem = {
-			window = {
-				mappings = {
-					["<C-b>"] = "close_window",
+		opts = {
+			filesystem = {
+				window = {
+					mappings = {
+						["<C-b>"] = "close_window",
+						["Y"] = function(state)
+							local node = state.tree:get_node()
+							local abs_path = vim.fn.fnamemodify(node:get_id(), ":p")
+							local root_path = vim.fn.fnamemodify(state.path or vim.loop.cwd(), ":p")
+							local rel_path = vim.fn.fnamemodify(abs_path, ":.")
+
+							if abs_path:sub(1, #root_path) == root_path then
+								rel_path = abs_path:sub(#root_path + 1)
+								if rel_path == "" then
+									rel_path = "."
+								end
+							end
+
+							vim.fn.setreg("+", rel_path)
+							vim.notify("Copied: " .. rel_path)
+						end,
+					},
 				},
 			},
 		},
-	},
-}
+	}
